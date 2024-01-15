5e6e8cea587f57476917620b4b636ce2fe3758a8
Duvan Castro <duvancastro1026@gmail.com>
2024-01-15 17:01:40 -0500

    added in log of the create pdf

--- PDFCreatorUI/Process/ImageFileProcessor.cs
+++ PDFCreatorUI/Process/ImageFileProcessor.cs
@@ -176,6 +176,9 @@ namespace PDFCreatorUI.Process
                     reader.Close();
                 }
             }
+
+            WriteInformationToFile(targetPDF);
+
             pdf.Close();
             pdfDoc.Close();
         }
